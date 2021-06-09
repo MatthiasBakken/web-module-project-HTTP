ab541d27a796c2963c5abab3a6212df2d97d44f7
Matthias Bakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-06-08 23:43:55 -0700

    completed project

--- client/src/App.js
+++ client/src/App.js
@@ -5,58 +5,66 @@ import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
-
+import AddMovieForm from "./components/AddMovieForm";
 import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
 
 import axios from 'axios';
 
-const App = (props) => {
-  const [movies, setMovies] = useState([]);
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:5000/api/movies')
-      .then(res => {
-        setMovies(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+const App = ( props ) => {
+  const [ movies, setMovies ] = useState( [] );
+  const [ favoriteMovies, setFavoriteMovies ] = useState( [] );
 
-  const deleteMovie = (id)=> {
-  }
+  useEffect( () => {
+    axios.get( 'http://localhost:5000/api/movies' )
+      .then( res => {
+        setMovies( res.data );
+      } )
+      .catch( err => {
+        console.log( err );
+      } );
+  }, [] );
 
-  const addToFavorites = (movie) => {
+  const deleteMovie = ( id ) => {
+  };
+
+  const addToFavorites = ( movie ) => {
     
-  }
+  };
 
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
+        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png" /> HTTP / CRUD Module Project</span>
       </nav>
 
       <div className="container">
-        <MovieHeader/>
+        <MovieHeader />
         <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
+          <FavoriteMovieList favoriteMovies={favoriteMovies} />
         
           <Switch>
-            <Route path="/movies/edit/:id">
-            </Route>
+            <Route path="/movies/edit/:id" render={props =>
+              <EditMovieForm {...props} setMovies={setMovies} />
+            }
+            />
 
-            <Route path="/movies/:id">
-              <Movie/>
-            </Route>
+            <Route path="/movies/add" render={props =>
+              <AddMovieForm {...props} setMovies={setMovies} />
+            }
+            />
+            <Route path="/movies/:id" render={props => 
+              <Movie {...props} setMovies={setMovies} />
+            }
+            />
 
             <Route path="/movies">
-              <MovieList movies={movies}/>
+              <MovieList movies={movies} />
             </Route>
 
             <Route path="/">
-              <Redirect to="/movies"/>
+              <Redirect to="/movies" />
             </Route>
           </Switch>
         </div>
